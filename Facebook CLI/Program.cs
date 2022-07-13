c977a36987bac39a87ee000f8c1da6a9bd8801ac
Ibne Nahian <evilprince2009@gmail.com>
2022-07-13 14:20:42 +0600

    user agent init

--- Facebook CLI/Program.cs
+++ Facebook CLI/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Playwright;
+
+namespace Facebook_CLI
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            string url = "https://www.facebook.com";
+            using IPlaywright playwright = await Playwright.CreateAsync();
+            BrowserTypeLaunchOptions options = new()
+            {
+                Headless = true,
+                SlowMo = 2000
+            };
+            await using IBrowser browser = await playwright.Firefox.LaunchAsync(options);
+            IBrowserContext context = await browser.NewContextAsync();
+            IPage page = await context.NewPageAsync();
+            await page.GotoAsync(url);
+            string title = await page.TitleAsync();
+            Console.WriteLine($"{title.Split(" ")[0]} loaded.");
+        }
+    }
+}
